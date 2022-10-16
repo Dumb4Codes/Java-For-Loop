6511c9b1d2e8fa85ee54f12451669e974fbd900d
Youvraj Singh <114943098+Dumb4Codes@users.noreply.github.com>
2022-10-16 14:24:27 +0530

    Add files via upload

---
+++ Z5_ForLoop.java
@@ -0,0 +1,19 @@
+
+public class Z5_ForLoop {
+
+	public static void main(String[] args) {
+		
+		
+		// for loop = executes a block of code for a limited amount of time
+		
+		// for loop has total of 3 statements index, condition and increment or decrement
+		
+		for(int i = 10; i>=1; i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Merry Christmas!");
+
+	}
+
+}
